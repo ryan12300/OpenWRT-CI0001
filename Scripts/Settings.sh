4d3bcaa18d83548e8b45939022efcba567a5ad5b
ryan12300 <70836399+ryan12300@users.noreply.github.com>
2026-03-02 20:03:44 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -29,7 +29,7 @@ fi
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
 #修改默认IP地址
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.10.251/g" $CFG_FILE
 #修改默认主机名
 sed -i "s/hostname='.*'/hostname='QCAWRT'/g" $CFG_FILE
 
